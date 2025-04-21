240b2d47aa4642714df66490180876457aeda254
Дамир <suleimanovdamir21@gmail.com>
2025-04-21 13:56:53 +0300

    Homework 3: all functionality done, final update

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,120 +1,124 @@
+import React, { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MenuItem from "../components/MenuItem";
 import Tooltip from "../components/Tooltip";
-import menuItems from "../data/menuItems";
 import "./menu.css";
 
-function MenuPage() {
-  return (
-    <>
-      <Header />
-      <main className="menu-wrapper">
-        <div className="menu-container">
-          <div className="menu-inner">
-            <h2 className="menu-title">Browse our menu</h2>
-            <p className="menu-subtitle">
-              Use our menu to place an order online, or{" "}
-              <Tooltip tooltipText="+375-44-457-61-57">
-                <span style={{ color: "#35B8BE" }}>phone</span>
-              </Tooltip>{" "}
-              our store <br /> to place a pickup order. Fast and fresh food.
-            </p>
+class MenuPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meals: [],
+      visibleCount: 6,
+      cart: {},
+    };
+  }
 
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                gap: "30px",
-                marginBottom: "40.91px",
-              }}
-            >
-              <button
-                style={{
-                  width: "147px",
-                  height: "52px",
-                  borderRadius: "6px",
-                  backgroundColor: "#35B8BE",
-                  border: "none",
-                  color: "#FFFFFF",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                  lineHeight: "20px",
-                }}
-              >
-                Desert
-              </button>
+  componentDidMount() {
+    fetch("https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Проверка первого item:", data[0]);
+        this.setState({ meals: data });
+      });
+  }
 
-              <button
-                style={{
-                  width: "132px",
-                  height: "52px",
-                  borderRadius: "6px",
-                  backgroundColor: "transparent",
-                  border: "1px solid #61728333",
-                  color: "#222222",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                  lineHeight: "20px",
-                }}
-              >
-                Dinner
-              </button>
+  handleAddToCart = (id) => {
+    this.setState((prevState) => {
+      const currentQty = prevState.cart[id] || 0;
+      return {
+        cart: {
+          ...prevState.cart,
+          [id]: currentQty + 1,
+        },
+      };
+    });
+  };
 
-              <button
-                style={{
-                  width: "137px",
-                  height: "52px",
-                  borderRadius: "6px",
-                  backgroundColor: "transparent",
-                  border: "1px solid #61728333",
-                  color: "#222222",
-                  fontSize: "16px",
-                  fontWeight: "400",
-                  lineHeight: "20px",
-                }}
-              >
-                Breakfast
-              </button>
-            </div>
+  handleSeeMore = () => {
+    this.setState((prevState) => ({
+      visibleCount: prevState.visibleCount + 6,
+    }));
+  };
 
-            <div className="menu-items">
-              {menuItems.map((item) => (
-                <MenuItem key={item.id} item={item} />
-              ))}
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "65px",
-                marginBottom: "148px",
-              }}
-            >
-              <button
-                style={{
-                  width: "147px",
-                  height: "52px",
-                  borderRadius: "6px",
-                  backgroundColor: "#35B8BE",
-                  border: "none",
-                  color: "#FFFFFF",
-                  fontSize: "17px",
-                  lineHeight: "20px",
-                  fontWeight: 400,
-                  fontFamily: "Inter, sans-serif",
-                  cursor: "pointer",
-                }}
-              >
-                See more
-              </button>
+  render() {
+    const { meals, visibleCount, cart } = this.state;
+    const visibleItems = meals.slice(0, visibleCount);
+    const totalItems = Object.values(cart).reduce((sum, qty) => sum + qty, 0);
+
+    return (
+      <>
+        <Header totalItems={totalItems} />
+        <main className="menu-wrapper">
+          <div className="menu-container">
+            <div className="menu-inner">
+              <h2 className="menu-title">Browse our menu</h2>
+              <p className="menu-subtitle">
+                Use our menu to place an order online, or{" "}
+                <Tooltip tooltipText="+375-44-457-61-57">
+                  <span style={{ color: "#35B8BE" }}>phone</span>
+                </Tooltip>{" "}
+                our store <br /> to place a pickup order.
+              </p>
+
+              <div className="menu-buttons">
+                <button className="menu-btn active">Dessert</button>
+                <button className="menu-btn">Dinner</button>
+                <button className="menu-btn">Breakfast</button>
+              </div>
+
+              <div className="menu-items">
+                {visibleItems.map((item) => (
+                  <MenuItem
+                    key={item.id}
+                    title={item.meal}
+                    image={item.img}
+                    price={item.price}
+                    area={item.area}
+                    category={item.category}
+                    quantity={cart[item.id] || 0}
+                    onAddToCart={() => this.handleAddToCart(item.id)}
+                  />
+                ))}
+              </div>
+
+              {visibleCount < meals.length && (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "65px",
+                    marginBottom: "148px",
+                  }}
+                >
+                  <button
+                    style={{
+                      width: "147px",
+                      height: "52px",
+                      borderRadius: "6px",
+                      backgroundColor: "#35B8BE",
+                      border: "none",
+                      color: "#FFFFFF",
+                      fontSize: "17px",
+                      lineHeight: "20px",
+                      fontWeight: 400,
+                      fontFamily: "Inter, sans-serif",
+                      cursor: "pointer",
+                    }}
+                    onClick={this.handleSeeMore}
+                  >
+                    See more
+                  </button>
+                </div>
+              )}
             </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-    </>
-  );
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default MenuPage;
