303265ba6c207c1635811460bc21e534edfcbdc6
Дамир <suleimanovdamir21@gmail.com>
2025-04-21 13:56:53 +0300

    Homework 3: all functionality done, final update

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,34 +1,54 @@
-import { useState } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./MenuItem.css";
 
-function MenuItem({ item }) {
-  const [quantity, setQuantity] = useState(1);
+class MenuItem extends Component {
+  render() {
+    const { image, title, price, onAddToCart, quantity, area, category } =
+      this.props;
 
-  return (
-    <div className="menu-item">
-      <img src={item.image} alt={item.name} className="menu-item-image" />
+    return (
+      <div className="menu-item">
+        <img src={image} alt={title} className="menu-item-image" />
+        <div className="menu-item-content">
+          <div className="menu-item-header">
+            <h3 className="menu-item-title">{title}</h3>
+            <span className="menu-item-price">${price.toFixed(2)} USD</span>
+          </div>
 
-      <div className="menu-item-content">
-        <div className="menu-item-header">
-          <h3 className="menu-item-title">{item.name}</h3>
-          <div className="menu-item-price">${item.price.toFixed(2)} USD</div>
-        </div>
+          <p className="menu-item-description">
+            {area && category
+              ? `${area} ${category}`
+              : category || area || "Meal"}
+          </p>
 
-        <p className="menu-item-description">{item.description}</p>
-
-        <div className="menu-item-actions">
-          <input
-            type="number"
-            min={1}
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            className="menu-item-qty"
-          />
-          <button className="menu-item-button">Add to cart</button>
+          <div className="menu-item-bottom">
+            <div className="menu-item-actions">
+              <input
+                className="menu-item-quantity"
+                type="text"
+                value={quantity}
+                readOnly
+              />
+              <button className="menu-item-button" onClick={onAddToCart}>
+                Add to cart
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+MenuItem.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
+  quantity: PropTypes.number,
+  onAddToCart: PropTypes.func,
+  area: PropTypes.string,
+  category: PropTypes.string,
+};
 
 export default MenuItem;
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./Tooltip.css";
 
-function Tooltip({ children, tooltipText }) {
-  const [show, setShow] = useState(false);
+class Tooltip extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
 
-  return (
-    <span
-      className="tooltip-wrapper"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      {children}
-      {show && <div className="tooltip-box">{tooltipText}</div>}
-    </span>
-  );
+  handleMouseEnter = () => {
+    this.setState({ show: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    const { children, tooltipText } = this.props;
+    const { show } = this.state;
+
+    return (
+      <span
+        className="tooltip-wrapper"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        {children}
+        {show && <div className="tooltip-box">{tooltipText}</div>}
+      </span>
+    );
+  }
 }
+
+Tooltip.propTypes = {
+  tooltipText: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 export default Tooltip;
