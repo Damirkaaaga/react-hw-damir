a9ba6c6ee71f4c21a5f3315d3b5eca0929a06d56
Дамир <suleimanovdamir21@gmail.com>
2025-05-16 15:11:24 +0300

    feat: complete Homework 6 with LoginPage, Firebase Auth, logger, and design fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import HomePage from "./pages/HomePage";
+import React from "react";
+import LoginPage from "./pages/Login/LoginPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Header />
+      <LoginPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
